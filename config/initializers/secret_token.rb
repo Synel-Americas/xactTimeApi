7bb8b54719b1869f9be631316466ff0993fe9234
chaol <ling.chao@gmail.com>
2014-03-31 09:55:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XactTimeApi::Application.config.secret_key_base = '004e0ebf5669681fe17df68926724c4996c018e3aeab115cb3cbb3d4dd87f6d45813bbb110000fc7e84369160903951e5eaf65a7396857774892ffbee5ef10d8'
